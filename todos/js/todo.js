cac32d03f1f78d27a776d9ef5033297aaa25757f
EtherealPatrick <yinghengsun@deepbaytech.com>
2017-12-21 18:41:07 +0800

    complete todos

--- todos/js/todo.js
+++ todos/js/todo.js
@@ -1,13 +1,25 @@
 $(function () {
 
     var todos = [];
-    function Todo(tid,title,completed){
+    function Todo(tid,title,checkState){
         this.tid = tid;
         this.title = title;
-        this.completed = completed;
-        this.sayHi = function(){
-            console.log('Hello,everyBody');
+        this.checkState = checkState;
+        this.printParams = function(){
+            console.log(this.tid);
+            console.log(this.title);
+            console.log(this.checkState);
         }
+    }
+
+    function setUnCheckedNum() {
+        var num = 0;
+        for (var i = 0;i<todos.length;i++){
+            if (todos[i].checkState === false){
+                num++;
+            }
+        }
+        $('.todo-count').children('strong').text(num);
     }
 
     function todoString(todo) {
@@ -17,18 +29,28 @@ $(function () {
                     "<label>"+todo.title+"</label>"+
                     "<button class='destroy'></button>"
                 "</div>"+
-                "<input class='edit' value="+todo.title+"/>"+
+                "<input class='edit' type='text' value="+todo.title+"/>"+
             "</li>";
 
     }
+
+    function getTodoIndex(temp) {
+        for (var i=0;i<todos.length;i++){
+            if (todos[i].tid == temp){
+                return i;
+            }
+        }
+        return -1;
+    }
     
     function render(todo) {
-        var todoStr = todoString(todo);
-        $('#todo-list').append($(todoStr));
-        $('#main').toggle(todos.length > 0);
-        // $('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
-        // this.renderFooter();
-        $('#new-todo').focus();
+        var todoStr = $(todoString(todo));
+        // console.log(todoStr.attr("id"));
+        $('.todo-list').append(todoStr);
+        $('.main').toggle(todos.length > 0);
+        $('.toggle-all').prop('checked', todos.length === 0);
+        $('.new-todo').focus();
+        setUnCheckedNum();
     }
 
     $(document).keydown(function (e) {
@@ -36,61 +58,90 @@ $(function () {
         var input = $(e.target);
         var val = input.val().trim();
 
-        if (keyCode !== 13 || !val) {
+        if (keyCode!== 13){
             return;
+        }else {
+            if(!val){
+                console.log("请输入内容...");
+                alert("请输入内容...");
+                return;
+            }
         }
 
-        console.log(1);
         var todo = new Todo(new Date().getTime(),val,false);
-
+        // todo.printParams();
         todos.push(todo);
 
         input.val('');
 
-        // render(todo);
-        var todoStr =  "<li id="+todo.tid+">"+
-            "<div class='view'>" +
-            "<input class='toggle' type='checkbox'/>"+
-            "<label>"+todo.title+"</label>"+
-            "<button class='destroy'></button>"
-        "</div>"+
-        "<input class='edit' value="+todo.title+"/>"+
-        "</li>";
-        var aObj=$("<li>百度</li>");
-        console.log(2);
-        $('#todo-list').append(aObj);
-        console.log(3);
-        $('#main').toggle(true);
-        $('#new-todo').focus();
-        console.log(4);
-
+        render(todo);
 
     });
 
-    // $('#new-todo').on('keyup', function (event) {
-    //     var input = $(event.target);
-    //     var val = input.val().trim();
-    //
-    //     if (event.which !== 13 || !val) {
-    //         return;
-    //     }
-    //
-    //     console.log(1);
-    //     var todo = new Todo(new Date().getTime(),val,false);
-    //
-    //     todos.push(todo);
-    //
-    //
-    //     input.val('');
-    //
-    //     render(todo);
-    // });
     // $('#toggle-all').on('change', this.toggleAll.bind(this));
     // $('#footer').on('click', '#clear-completed', this.destroyCompleted.bind(this));
-    // $('#todo-list')
-    //     .on('change', '.toggle', this.toggle.bind(this))
-    //     .on('dblclick', 'label', this.editingMode.bind(this))
-    //     .on('keyup', '.edit', this.editKeyup.bind(this))
-    //     .on('focusout', '.edit', this.update.bind(this))
-    //     .on('click', '.destroy', this.destroy.bind(this));
+    $('.todo-list')
+        .on('change', '.toggle', function (e) {
+            var ce = e.target;
+            var aid = $(ce).closest("li").attr('id');
+            var index = getTodoIndex(aid);
+            console.log(index);
+            console.log($('.toggle').prop('checked'));
+            todos[index].checkState = !todos[index].checkState;
+            if (todos[index].checkState){
+                $(ce).closest("li").addClass('completed');
+            }else {
+                $(ce).closest("li").removeClass('completed');
+            }
+
+            setUnCheckedNum();
+
+        })
+        .on('dblclick', 'label', function (e) {
+
+        })
+        .on('click', '.destroy', function (e) {
+            var aid = $(e.target).closest("li").attr('id');
+
+            $(e.target).closest('li').remove();
+
+            var index = getTodoIndex(aid);
+            todos.splice(index,1);
+
+            setUnCheckedNum();
+        });
+    $('.toggle-all').on('click',function () {
+        var flag=$(".toggle-all").prop("checked");
+        for (var i=0;i<todos.length;i++){
+             todos[i].checkState = flag;
+        }
+        $('ul>li').each(function (index, element) {
+            $(element).children('div').find('.toggle').prop("checked",flag);
+            console.log($(element).children('div').find('.toggle').prop("checked"));
+            if ($(element).hasClass('completed')){
+                if (!flag){
+                    $(element).removeClass('completed');
+                }
+            }else {
+                if(flag){
+                    $(element).addClass('completed');
+                }
+            }
+        });
+
+        setUnCheckedNum();
+
+    });
+
+    $('.clear-All').on('click',function () {
+        // $(".todo-list>li").last("li").hide(800,function () {
+        //     $(this).prev().hide(800,arguments.callee);
+        // });
+        // $(".todo-list>").html("");
+        $(".todo-list>li").each(function (index, element) {
+            element.remove();
+        });
+        todos = [];
+        setUnCheckedNum();
+    });
 });
